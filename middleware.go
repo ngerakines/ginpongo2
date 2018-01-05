e6bf7b2ddefb3e23a2bca0119903e347dc790843
Nick Gerakines <nick@gerakines.net>
2018-01-05 09:32:05 -0500

    Refactored to improve error handling

--- middleware.go
+++ middleware.go
@@ -4,24 +4,47 @@ import (
 	"github.com/flosch/pongo2"
 	. "github.com/gin-gonic/gin"
 	"net/http"
+	"fmt"
 )
 
 func Pongo2() HandlerFunc {
 	return func(c *Context) {
 		c.Next()
 
-		templateName, templateNameError := c.Get("template")
-		templateNameValue, isString := templateName.(string)
+		name, err := stringFromContext(c, "template")
+		if err != nil {
+			http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
+		}
 
-		if templateNameError == nil && isString {
-			templateData, templateDataError := c.Get("data")
-			var template = pongo2.Must(pongo2.FromFile(templateNameValue))
-			err := template.ExecuteWriter(getContext(templateData, templateDataError), c.Writer)
-			if err != nil {
-				http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
-			}
+		data, _ := c.Get("data")
+
+		template := pongo2.Must(pongo2.FromFile(name))
+		err = template.ExecuteWriter(convertContext(data), c.Writer)
+		if err != nil {
+			http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
 		}
 	}
+}
+
+func stringFromContext(c *Context, input string) (string, error) {
+	raw, ok := c.Get(input)
+	if ok {
+		strVal, ok := raw.(string)
+		if ok {
+			return strVal, nil
+		}
+	}
+	return "", fmt.Errorf("No data for context variable: %s", input)
+}
+
+func convertContext(thing interface{}) pongo2.Context {
+	if thing != nil {
+		context, isMap := thing.(map[string]interface{})
+		if isMap {
+			return context
+		}
+	}
+	return nil
 }
 
 func getContext(templateData interface{}, err error) pongo2.Context {
